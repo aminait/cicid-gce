f4143a1ccc21e9ccd6ec930c5e05f09325181dc9
Amina Ait <aminait@outlook.com>
2021-10-02 21:26:06 +0300

    Testing out code update

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.json("Don't disappoint me CI/CD World");
+    res.json("Don't disappoint me CI/CD World PLEASE");
 });
 
 app.get('/test', (req, res) => {
     res.json('Passed!');
+});
+
+app.get('/update', (req, res) => {
+    res.json('Am I ever going to see the light of a screen?');
 });
 
 app.listen(PORT);
