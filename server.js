a455da2abdb3d4f07a7a19cc6a95c04889127132
Amina Ait <aminait@outlook.com>
2021-10-02 22:45:19 +0300

    Add bad joke

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.get('/please', (req, res) => {
     res.json('Please...');
 });
 
-
-// app.get('/update', (req, res) => {
-//     res.json('Am I ever going to see the light of a screen?');
-// });
+app.get('/joke', (req, res) => {
+    res.json("What do you call a gentleman-ly server? SIR-ver");
+});
 
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
